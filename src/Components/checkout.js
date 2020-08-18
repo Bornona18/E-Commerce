309f2204e83f330afd1fc51f19f5e9290d56dcf2
Bornona18 <61803232+Bornona18@users.noreply.github.com>
2020-08-18 19:20:07 -0400

    Add files via upload

---
+++ src/Components/checkout.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+
+
+//  statefull Component
+ class checkout extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>  Thans For Shopping With Us.......
+            </h1>
+                <br /><br /><br />
+                <h3>Have a great Day...</h3>
+            </div>
+        )
+    }
+}
+
+export default checkout
