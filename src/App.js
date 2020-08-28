e28c93dc200e9cd3777d4001e5acda2f21505dac
Bornona18 <61803232+Bornona18@users.noreply.github.com>
2020-08-28 14:54:49 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
               <hr class="clearfix w-100 d-md-none pb-3"></hr>
               <div class="col-md-6 ">
                 <h5 >About Us</h5>
-                <p>GiganticDesire is a USA e-commerce company based in Bronx, N.Y, USA. It was founded by Nusrat  in 2007.</p>
+                <p>GiganticDesire is a USA e-commerce company based in Bronx, N.Y, USA. It was founded by Nusrat J Bornona in 2020.</p>
               </div>
               <hr class="clearfix w-100 d-md-none pb-3"></hr>
               <div class="col-md-3">
